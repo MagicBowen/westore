8f42d912ec273e06cd8d61ddec7d8bc438c96b56
尉刚强 <wei.gangqiang@zte.com.cn>
2017-12-12 09:55:05 +0800

    add picture for size conversion

--- westore_server.py
+++ westore_server.py
@@ -143,6 +143,11 @@ def download_picture():
     image_id = request.args.get('pictureid')
     return send_file("./intramirror/{}/target/{}".format(product_id, image_id), mimetype='image/jpeg')
 
+@app.route('/westore/picture/size/relation', methods=['GET'])
+def download_size_relation():
+    relation_Name = request.args.get('relationName')
+    return send_file("./sizeRelation/{}".format(relation_Name), mimetype='image/jpeg')
+
 @app.route('/')
 def index():
     return render_template('index.html')
