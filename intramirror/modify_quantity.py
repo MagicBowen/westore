1bf4ac13fb1e26b1f2ff49f47c8156a1bcb41185
MagicBowen <e.bowen.wang@icloud.com>
2017-12-16 10:09:12 +0800

    modify quantity

--- intramirror/modify_quantity.py
+++ intramirror/modify_quantity.py
@@ -22,5 +22,5 @@ def modify_quantity(input_folder, number_ceil):
 
 if __name__ == '__main__':
     input_folder = '.'
-    number_ceil = 100
+    number_ceil = 300
     modify_quantity(input_folder, number_ceil)
